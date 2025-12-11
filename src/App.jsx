351512c3d9945994043960ba1dff434482bb53be
Balraj Choure <balrajchoure09@email.com>
2025-12-11 16:06:10 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div>
+      <h1 className="bg-blue-50 text-center text-2xl p-4 font-bold text-neutral-400">Solana Airdrop Faucet</h1>
+    </div>
+  )
+}
+
+export default App
